942e966013957cdf5d512a78ba18f5801f457be0
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-19 19:29:28 -0600

    Add Header and Empty components to the Appointment components. Create stories for Header and Empty components

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,9 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 
 storiesOf("Button", module)
@@ -40,7 +43,7 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
   ));
 
 
-  storiesOf("DayList", module)
+storiesOf("DayList", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
   })
@@ -116,3 +119,12 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
