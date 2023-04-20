d3f866e2bdb63d6b9bc0fc922093d54053e4e30f
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-20 10:19:34 -0600

    implement Show if an appointment was booked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,11 +5,10 @@ import Show from "./Show";
 import Empty from "./Empty";
 
 export default function Appointment(props) {
-  const {student, interviewer} = props
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={student} interviewer={interviewer}/> : <Empty />}
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
     </article>
   );
 }
