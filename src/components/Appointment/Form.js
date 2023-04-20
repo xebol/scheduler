f9ac2741893f974fe4a8f529be66925e4e87b71c
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-20 09:40:39 -0600

    Implement the Create stories that changes the state of the Student and Interview when an interview is being created

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function() {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = function() {
+    reset();
+    props.onCancel();
+  };
+
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off"
+            onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
             type="text"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-            your code goes here
-          */
           />
         </form>
         <InterviewerList
-        /* your code goes here */
+          interviewers={props.interviewers}
+          value={interviewer}
+          setInterviewer={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
