5168ead8f93c40a6e56a2f72c5f8dd6af8d0ad06
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-20 18:48:34 -0600

    Request data using axios from scheduler-api

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
+
 export default function Appointment(props) {
   return (
     <article className="appointment">
